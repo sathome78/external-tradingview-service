424ae4e1732bbd83d96475ea879df1545609c24e
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-08-22 00:32:38 +0300

    start implement

---
+++ src/main/java/me/exrates/externalservice/controllers/DataIntegrationController.java
@@ -0,0 +1,10 @@
+package me.exrates.externalservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DataIntegrationController {
+
+}
