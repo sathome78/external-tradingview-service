3bcbb77282ccc9f8ae732f930cfeef6126362dc0
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-08-23 04:15:15 +0300

    min fix
    (cherry picked from commit 049b3bdeb7a305ddca4c334c5663ef80bcac0d36)

--- src/main/java/me/exrates/externalservice/dto/QuotesDto.java
+++ src/main/java/me/exrates/externalservice/dto/QuotesDto.java
@@ -7,59 +7,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.exrates.externalservice.api.ExratesPublicApi;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 @Data
 @Builder(builderClassName = "Builder", toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuotesDto {
 
-    @JsonProperty("ch")
-    private BigDecimal change;
-    @JsonProperty("chp")
-    private BigDecimal changePercent;
-    @JsonProperty("lp")
-    private BigDecimal lastPrice;
-    @JsonProperty("ask")
-    private BigDecimal askPrice;
-    @JsonProperty("bid")
-    private BigDecimal bidPrice;
-    @JsonProperty("open_price")
-    private BigDecimal openPrice;
-    @JsonProperty("high_price")
-    private BigDecimal highPrice;
-    @JsonProperty("low_price")
-    private BigDecimal lowPrice;
-    @JsonProperty("prev_close_price")
-    private BigDecimal previousClosePrice;
-    private BigDecimal volume;
-    private int buyPipValue;
-    private int sellPipValue;
+    private static final String OK = "ok";
 
-    public static QuotesDto of(ExratesPublicApi.TickerResponse response) {
-        final double last = response.getLast().doubleValue();
-        final double percentChange = response.getPercentChange().doubleValue();
-        final double changeDouble = last * percentChange / 100;
-        final double previousLastDouble = last - changeDouble;
+    @JsonProperty("s")
+    private String status;
+    @JsonProperty("n")
+    private String pair;
+    @JsonProperty("v")
+    private PriceDto price;
 
-        final BigDecimal change = BigDecimal.valueOf(changeDouble).setScale(8, RoundingMode.HALF_UP);
-        final BigDecimal previousLast = BigDecimal.valueOf(previousLastDouble).setScale(8, RoundingMode.HALF_UP);
-
+    public static QuotesDto of(String pair, ExratesPublicApi.TickerResponse response) {
         return QuotesDto.builder()
-                .change(change)
-                .changePercent(response.getPercentChange())
-                .lastPrice(response.getLast())
-                .askPrice(response.getLowestAsk())
-                .bidPrice(response.getHighestBid())
-                .openPrice(previousLast)
-                .highPrice(response.getHigh())
-                .lowPrice(response.getLow())
-                .previousClosePrice(previousLast)
-                .volume(response.getBaseVolume())
-                .buyPipValue(1)
-                .sellPipValue(1)
+                .status(OK)
+                .pair(pair)
+                .price(PriceDto.of(response))
                 .build();
     }
 }
