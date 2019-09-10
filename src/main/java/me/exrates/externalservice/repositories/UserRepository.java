5ae193678dbb190ac3000c58f655f4ed272903c2
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-09-10 05:50:23 +0300

    delete all

--- src/main/java/me/exrates/externalservice/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package me.exrates.externalservice.repositories;
-
-import me.exrates.externalservice.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByLogin(String login);
-}
--- src/main/java/me/exrates/externalservice/web/security/UserDetailsNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package me.exrates.externalservice.web.security;
-
-class UserDetailsNotFoundException extends Exception {
-
-    UserDetailsNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
